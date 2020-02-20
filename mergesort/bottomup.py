9b70d2e92631d0f1459a637f04d2fd8e718d4925
Felipe Guilherme <felipeguilhermefs@gmail.com>
2020-02-19 22:19:40 -0300

    Extracts merge algorithm

--- mergesort/bottomup.py
+++ mergesort/bottomup.py
@@ -1,3 +1,5 @@
+from mergesort.merge import merge
+
 # Mergesort Bottom-Up implementation
 def mergesort(arr):
     if len(arr) < 2:
@@ -9,28 +11,6 @@ def mergesort(arr):
     while width < size:
 
         for offset in range(0, size, width * 2):
-            __merge(arr, offset, min(offset + width, size), min(offset + 2 * width, size))
+            merge(arr, offset, min(offset + width, size), min(offset + 2 * width, size))
         
         width *= 2
-
-def __merge(arr, begin, middle, end):
-    # indexes to keep track of each half
-    li = 0
-    ri = 0
-
-    # halves to merge
-    left = arr[begin:middle]
-    right = arr[middle:end]
-
-    max_left = len(left)
-    max_right = len(right)
-
-    # Each iteration will choose the halves min element
-    # its easy since they are already sorted o.o
-    for i in range(begin, end):
-        if li < max_left and (ri >= max_right or left[li] <= right[ri]):
-            arr[i] = left[li]
-            li += 1
-        else:
-            arr[i] = right[ri]
-            ri += 1
--- mergesort/topdown.py
+++ mergesort/topdown.py
@@ -1,3 +1,5 @@
+from mergesort.merge import merge
+
 # Mergesort Top-Down implementation
 def mergesort(arr):
     __mergesort(arr, 0, len(arr))
@@ -12,26 +14,5 @@ def __mergesort(arr, start, end):
     __mergesort(arr, start, middle)
     __mergesort(arr, middle, end)
     
-    __merge(arr, start, middle, end)
+    merge(arr, start, middle, end)
 
-def __merge(arr, begin, middle, end):
-    # indexes to keep track of each half
-    li = 0
-    ri = 0
-
-    # halves to merge
-    left = arr[begin:middle]
-    right = arr[middle:end]
-
-    max_left = len(left)
-    max_right = len(right)
-
-    # Each iteration will choose the halves min element
-    # its easy since they are already sorted o.o
-    for i in range(begin, end):
-        if li < max_left and (ri >= max_right or left[li] <= right[ri]):
-            arr[i] = left[li]
-            li += 1
-        else:
-            arr[i] = right[ri]
-            ri += 1
